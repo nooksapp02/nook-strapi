60f025d96911647728792dc0bea180624e91eba2
nooksapp02 <iescudero@soluprime.com.pa>
2023-06-08 14:02:26 -0500

    spaceadd

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,4 +9,30 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
+          ],
+          upgradeInsecureRequests: null,
+
+        },
+      },
+    },
+  },
 ];
