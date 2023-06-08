95dc65e3580740233e5c7223569f3948c795893c
nooksapp02 <iescudero@soluprime.com.pa>
2023-06-08 12:39:53 -0500

    database production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,17 @@
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
-        rejectUnauthorized: false,
+        ca: env('DATABASE_CA')
       },
     },
-      debug: false,
+    debug: false,
   },
 });
